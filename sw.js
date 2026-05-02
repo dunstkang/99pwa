2ed7ccc2d07bbaff92d1cc3aea127eca6a621e62
Dunst Kang <dunst.kang@gmail.com>
2026-05-02 17:38:54 +0800

    Update manifest and bump SW version to v2

--- sw.js
+++ sw.js
@@ -1,4 +1,6 @@
-const CACHE_NAME = '99-pwa-v1';
+// 每次程式碼大改後，手動把 v1 改成 v2, v3...
+const CACHE_NAME = '99-pwa-v2'; 
+
 const ASSETS = [
   'index.html',
   'manifest.json',
@@ -6,16 +8,10 @@ const ASSETS = [
   'https://pyscript.net/releases/2024.1.1/core.js'
 ];
 
-// 安裝時快取檔案
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
-// 攔截請求，優先從快取抓取
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
